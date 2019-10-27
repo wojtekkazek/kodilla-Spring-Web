02af002d07afbbd60008247f3b911878a0495149
wojtekkazek <wojciech.kazek@gmail.com>
2019-10-27 21:51:34 +0000

    18.3

---
+++ src/main/java/com/crud/tasks2/Tasks2Application.java
@@ -0,0 +1,27 @@
+package com.crud.tasks2;
+
+import com.crud.tasks2.domain.TaskDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
+
+@SpringBootApplication
+public class Tasks2Application {
+
+	public static void main(String[] args) {
+		TaskDto taskDto = new TaskDto(
+				(long)1,
+				"Test title",
+				"I want to be a coder");
+
+		Long id = taskDto.getId();
+		String title = taskDto.getTitle();
+		String content = taskDto.getContent();
+
+		System.out.println(id + " " + title + " " + content);
+
+		//SpringApplication.run(Tasks2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/tasks2/Tasks2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.tasks2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tasks2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
