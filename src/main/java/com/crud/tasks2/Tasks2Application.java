8a33ca0ef1e969a748f1585fc487694188603c05
wojtekkazek <wojciech.kazek@gmail.com>
2019-11-04 22:29:10 +0000

    20.3

--- src/main/java/com/crud/tasks2/Tasks2Application.java
+++ src/main/java/com/crud/tasks2/Tasks2Application.java
@@ -3,11 +3,13 @@ package com.crud.tasks2;
 import com.crud.tasks2.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.awt.*;
 
 @SpringBootApplication
-public class Tasks2Application {
+public class Tasks2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 //		TaskDto taskDto = new TaskDto(
@@ -22,6 +24,11 @@ public class Tasks2Application {
 //		System.out.println(id + " " + title + " " + content);
 
 		SpringApplication.run(Tasks2Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Tasks2Application.class);
 	}
 
 }
